af3f6d9900855a04aa4636eea80311625d5c6991
RobertAKARobin <contact@robertakarobin.com>
2023-03-19 10:49:53 -0500

    download deno types

---
+++ prelint.js
@@ -0,0 +1,20 @@
+const fs = require(`fs`);
+const sys = require(`child_process`);
+
+const denoTypesPath_remote_fromVersion = denoVersion =>
+`https://github.com/denoland/deno/releases/download/v${denoVersion}/lib.deno.d.ts`;
+const denoTypesPath_local = `deno.types.d.ts`;
+
+(async function() {
+	const denoVersion = await new Promise((resolve) => sys // TODO config deno version
+		.exec(`deno eval 'console.log(Deno.version.deno)'`)
+		.stdout
+		.on(`data`, data => resolve(data.trim()))
+	);
+	const denoTypesPath_remote = denoTypesPath_remote_fromVersion(denoVersion);
+	const denoTypesResponse = await fetch(denoTypesPath_remote);
+	const denoTypes = await denoTypesResponse.text();
+	fs.writeFileSync(denoTypesPath_local, denoTypes);
+
+	await sys.exec(`deno vendor serve.ts`); // TODO config vendor
+})();

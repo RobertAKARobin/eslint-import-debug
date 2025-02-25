2accc340fee5fa9508d6446c49f35a35886b1a41
RobertAKARobin <contact@robertakarobin.com>
2025-02-25 11:26:58 -0600

    wip attr

--- index.js
+++ index.js
@@ -19,20 +19,47 @@
  * @template {() => any} Fun
  */
 
-const suffix = /** @type {const} */(`Event`);
+const attrSuffix = /** @type {const} */(`_attr`);
+const eventSuffix = /** @type {const} */(`_event`);
 
 /**
- * @typedef {typeof suffix} Suffix
+ * @typedef {typeof attrSuffix} AttrSuffix
+ * @typedef {typeof eventSuffix} EventSuffix
  */
 
 class CanNotify extends EventTarget {
+	/**
+	 * @type {Array<string>}
+	 * @readonly
+	 */
+	static observedAttributes = [];
+
+	/**
+	 * @template Value
+	 * @param {Value} initial
+	 * @returns {Value & { attribute: true }}
+	 */
+	static attribute(initial) {
+		console.log(this.attribute);
+		return /** @type {Value & { attribute: true }} */(initial);
+	}
+
+	/**
+	 * @template {keyof this} AttrKey
+	 * @param {AttrKey} attrKey
+	 * @returns {this}
+	 */
+	changes(attrKey) {
+		return this;
+	}
+
 	/**
 	 * @template {keyof this} EventKey
 	 * @template {EventTarget & Record<EventKey, () => any>} Origin
 	 * @template {ReturnType<Origin[EventKey]>} EventDetail
 	 * @template Listener
 	 * @template {keyof Listener} HandlerKey
-	 * @param {EventKey extends `${string}${Suffix}` ? EventKey : never} eventKey
+	 * @param {EventKey extends `${string}${EventSuffix}` ? EventKey : never} eventKey
 	 * @param {Listener} listener
 	 * @param {Listener[HandlerKey] extends ((event: CustomEvent<EventDetail>) => any) ? HandlerKey : never} handlerKey
 	 * @returns {Origin}
@@ -60,37 +87,56 @@ class DiceCounter {
 }
 
 class Dice extends CanNotify {
-	rollEvent() {
+	get poo_attr() { return 3 }
+	set poo_attr(value) {}
+
+	roll_event() {
 		 return 1 + Math.round(Math.random() * 5);
 	}
 }
 
 const prototypeProperties = Object.getOwnPropertyDescriptors(Dice.prototype);
 for (const prototypePropertyName in prototypeProperties) {
-	if (prototypePropertyName.endsWith(suffix) === false) { // Brittle AF. At least we can be pretty certain these are methods and not properties bc properties usually aren't defined on prototype?
-		continue;
+	if (prototypePropertyName.endsWith(eventSuffix)) { // Brittle AF. At least we can be pretty certain these are methods and not properties bc properties usually aren't defined on prototype?
+		// @ts-expect-error We know this funciton exists
+		const fun = Dice.prototype[prototypePropertyName];
+		Object.assign(Dice.prototype, {
+			/**
+			 * @this {CanNotify}
+			 */
+			[prototypePropertyName](/** @type {any} */...args) {
+				const detail = fun.apply(this, args);
+				this.dispatchEvent(new CustomEvent(prototypePropertyName, { detail }));
+				return detail;
+			},
+		});
 	}
 
-	// @ts-expect-error We know this funciton exists
-	const fun = Dice.prototype[prototypePropertyName];
-	Object.assign(Dice.prototype, {
-		/**
-		 * @this {CanNotify}
-		 */
-		[prototypePropertyName](/** @type {any} */...args) {
-			const detail = fun.apply(this, args);
-			this.dispatchEvent(new CustomEvent(prototypePropertyName, { detail }));
-			return detail;
-		},
-	});
+	if (prototypePropertyName.endsWith(attrSuffix)) {
+		const attributeName = prototypePropertyName;
+		const definition = prototypeProperties[prototypePropertyName];
+		Object.defineProperty(Dice.prototype, prototypePropertyName, {
+			...definition,
+			// get() {
+			// 	return this.getAttribute(attributeName);
+			// },
+			// set(value) {
+			// 	if (attributeValueIsEmpty(value)) {
+			// 		this.removeAttribute(attributeName);
+			// 	} else {
+			// 		this.setAttribute(attributeName, value.toString());
+			// 	}
+			// },
+		});
+	}
 }
 
 const diceCounter = new DiceCounter();
 const dice = new Dice()
-	.emits(`rollEvent`, diceCounter, `onRoll`);
+	.emits(`roll_event`, diceCounter, `onRoll`);
 
-const player1Roll = dice.rollEvent();
-const player2Roll = dice.rollEvent();
+const player1Roll = dice.roll_event();
+const player2Roll = dice.roll_event();
 
 console.log(diceCounter.sum, player1Roll, player2Roll);
 
